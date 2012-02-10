3bd5b2585cb41405c5a535d94346e6ee9a7f1856
dev@chilerocks.org <dev@chilerocks.org@32267626-9227-ed93-e84d-f69ef0d102f1>
2012-02-10 03:32:37 +0000

    first commit

---
+++ trunk/src/com/chilerocks/securitycamapp/SecurityCamAppActivity.java
@@ -0,0 +1,13 @@
+package com.chilerocks.securitycamapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecurityCamAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
